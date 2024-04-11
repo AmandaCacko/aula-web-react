80d7c420f7197d3e311880c844b67d0ed25c4263
amandacacko <amandacacko@gmail.com>
2024-04-11 10:57:49 -0300

    Exercícios resolvidos

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import ButtonFatec from './components/button-fatec'
+
+function App() {  
+  return (
+  <>
+    <p>mim de papai auuuuuuuuuuuuu</p> 
+    <ButtonFatec type='button' label='🐺'/>
+    <ButtonFatec type='button' label='🔈'/>
+  </>  
+  )
+}
+
+export default App
